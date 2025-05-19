a0cb25913023d728597fdbec054451c82f7e316c
zhouzhenhua <zhouzhenhua1984@163.com>
2025-05-19 17:15:21 +0800

    初次提交

--- zhouzh3-sugarcane-app/src/main/java/com/zhouzh3/sugarcane/app/service/impl/FeignServiceImpl.java
+++ zhouzh3-sugarcane-app/src/main/java/com/zhouzh3/sugarcane/app/service/impl/FeignServiceImpl.java
@@ -3,7 +3,6 @@ package com.zhouzh3.sugarcane.app.service.impl;
 import com.zhouzh3.common.response.R;
 import com.zhouzh3.sugarcane.api.feign.ScoutApi;
 import com.zhouzh3.sugarcane.app.service.FeignService;
-import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
