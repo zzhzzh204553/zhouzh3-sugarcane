6e08cc05db972c155a23939eadc4a62774fe370e
zhouzhenhua <zhouzhenhua1984@163.com>
2025-05-19 17:58:04 +0800

    初次提交

--- zhouzh3-sugarcane-app/src/main/java/com/zhouzh3/sugarcane/app/controller/ScoutController.java
+++ zhouzh3-sugarcane-app/src/main/java/com/zhouzh3/sugarcane/app/controller/ScoutController.java
@@ -1,18 +1,20 @@
 package com.zhouzh3.sugarcane.app.controller;
 
 import com.zhouzh3.common.response.R;
-import com.zhouzh3.sugarcane.api.feign.ScoutApi;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class ScoutController implements ScoutApi {
+public class ScoutController
+//        implements ScoutApi
+{
 
     @Value("${custom.message:world}")
     private String message;
 
-    @Override
+    @GetMapping("/api/scout/hello")
     public R<String> hello() {
         return R.success("hello " + message);
     }
