c49fc0b03096b8bd7f5a35029d1d9ff8da685eb4
zhouzhenhua <zhouzhenhua1984@163.com>
2025-05-20 11:20:10 +0800

    重启eureka服务

--- zhouzh3-sugarcane-boot/src/main/java/com/zhouzh3/sugarcane/boot/config/Jersey3TransportClientFactoriesConfig.java
+++ zhouzh3-sugarcane-boot/src/main/java/com/zhouzh3/sugarcane/boot/config/Jersey3TransportClientFactoriesConfig.java
@@ -1,13 +1,13 @@
 package com.zhouzh3.sugarcane.boot.config;
 
-import com.netflix.discovery.shared.transport.jersey3.Jersey3TransportClientFactories;
+//import com.netflix.discovery.shared.transport.jersey3.Jersey3TransportClientFactories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Jersey3TransportClientFactoriesConfig {
-    @Bean
-    public Jersey3TransportClientFactories jersey3TransportClientFactories() {
-        return new Jersey3TransportClientFactories();
-    }
+//    @Bean
+//    public Jersey3TransportClientFactories jersey3TransportClientFactories() {
+//        return new Jersey3TransportClientFactories();
+//    }
 }
