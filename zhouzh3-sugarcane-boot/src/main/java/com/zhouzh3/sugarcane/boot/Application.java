767687087b1e05459481113f5e7dc1f209681160
zhouzhenhua <zhouzhenhua1984@163.com>
2025-05-20 11:07:29 +0800

    重启eureka服务

--- zhouzh3-sugarcane-boot/src/main/java/com/zhouzh3/sugarcane/boot/Application.java
+++ zhouzh3-sugarcane-boot/src/main/java/com/zhouzh3/sugarcane/boot/Application.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = "com.zhouzh3.sugarcane.**")
-@EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.zhouzh3.sugarcane.api.feign")
+@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
